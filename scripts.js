730052149d7a4211cd6a1b7b1b90aed27eba6ab3
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2023-02-03 20:02:57 +0000

    Add createDivs function to create 16 divs, each containing another 16 divs

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 function createDivs() {
-    for(let i = 0; i < )
+    const container = document.querySelector(".container");
+    const rows = 16;
+    const cols = 16;
+
+
+    for (let i = 0; i < rows; i++) {
+        let row = document.createElement("div");
+        row.className = "row";
+        for (let j = 0; j < cols; j++) {
+            let gridSquare = document.createElement("div");
+            gridSquare.className = "gridsquare";
+            gridSquare.innerText = "0";
+            row.appendChild(gridSquare);
+        }
+        container.appendChild(row);
+    }
 }
+
+createDivs();
