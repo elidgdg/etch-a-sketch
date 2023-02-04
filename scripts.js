654a568b89a48551c47a3c59e642cced4d603de1
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2023-02-04 19:23:13 +0000

    Add draw function. Now only draws when mouse is held down

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Create a 16x16 grid of divs
 function createDivs() {
     const container = document.querySelector(".container");
     const rows = 16;
@@ -15,25 +16,28 @@ function createDivs() {
         container.appendChild(row);
     }
 }
-
+createDivs();
+// Set the color of an html element
 function setColor(element, color) {
     element.style.backgroundColor = color;
 }
 
-createDivs();
+function draw(pixel) {
+    if (!mouseDown) {return;}
+
+    setColor(pixel, 'red');
+}
 
 const gridSquares = document.querySelectorAll(".gridsquare");
-
-gridSquares.forEach((gridSquare) => {
-    gridSquare.addEventListener('mouseenter', () => setColor(gridSquare, 'red'));
-    gridSquare.addEventListener('mouseleave', () => setColor(gridSquare, 'black'));
-});
-
 const container = document.querySelector(".container");
+let mouseDown = false;
+
+// Listen for mouse entering/leaving a gridsquare
+gridSquares.forEach((gridSquare) => {
+    gridSquare.addEventListener('mouseenter', () => draw(gridSquare));
+});
+// Check if mouse is held down
 container.addEventListener('mousedown', () => (mouseDown = true));
 container.addEventListener('mouseup', () => (mouseDown = false));
 
-let mouseDown = false;
 
-container.addEventListener('mousedown', () => (console.log(mouseDown)));
-container.addEventListener('mouseup', () => (console.log(mouseDown)));
